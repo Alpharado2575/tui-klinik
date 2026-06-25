0cfef2266c5b7dafba88f3a1cd8f6135b204dd34
alphasekki <alphasekki@presenkolah.sch.id>
2026-06-25 11:05:08 +0700

    edit some stuff

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://02a7-103-120-173-31.ngrok-free.app";
+export const BASE_URL = "https://unclasp-grab-enamel.ngrok-free.dev";
 
 export const api = {
     login: async (usr: string, pwd: string) => {
